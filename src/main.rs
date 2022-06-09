50e228f77bae03fa5de680976b70b2acc6a589f4
pxp9 <pepe.marquezromero@gmail.com>
2022-06-10 01:57:29 +0200

    funciona el echo bot

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
 use std::env;
 
-use futures::StreamExt;
+use futures::stream::StreamExt;
 use telegram_bot::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let token = env::var("RUST_TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
+    let token = env::var("RUST_TELEGRAM_BOT_TOKEN").expect("RUST_TELEGRAM_BOT_TOKEN not set");
     let api = Api::new(token);
 
     // Fetch new updates via long poll method
     let mut stream = api.stream();
     while let Some(update) = stream.next().await {
-        // If the received update contains a new message...
-        let update = update?;
-        if let UpdateKind::Message(message) = update.kind {
+        if let UpdateKind::Message(ref message) = update.expect("fuck").kind {
             if let MessageKind::Text { ref data, .. } = message.kind {
                 // Print received text message to stdout.
                 println!("<{}>: {}", &message.from.first_name, data);
 
                 // Answer message with "Hi".
-                api.send(message.text_reply(format!(
-                    "Hi, {}! You just wrote '{}'",
-                    &message.from.first_name, data
-                )))
+                let username = message.from.username.as_ref().unwrap();
+                api.send(
+                    message
+                        .text_reply(format!("Hi, @{}! You just wrote '{}'", username, data))
+                        .parse_mode(ParseMode::Markdown),
+                )
                 .await?;
             }
         }
