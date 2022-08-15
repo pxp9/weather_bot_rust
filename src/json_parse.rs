e00aa6246f6faabf7883ac484bf46d7d4f27503c
pxp9 <48651252+pxp9@users.noreply.github.com>
2022-08-15 21:37:01 +0200

    Handle duplicated entries json

--- src/json_parse.rs
+++ src/json_parse.rs
@@ -22,16 +22,10 @@ pub async fn read_json_cities() -> Result<(), BotDbError> {
 
         // For each city check if it is in db, if not is in db, insert the city
         for city in cities {
-            let n = connection
-                .execute(
-                    "SELECT * FROM cities WHERE name = $1 AND country = $2 AND state = $3 LIMIT 1",
-                    &[&city.name, &city.country, &city.state],
-                )
-                .await?;
-            if n == 0 {
-                connection.execute("INSERT INTO cities (name , country , state , lon , lat ) VALUES ($1 , $2 , $3 , $4 , $5)"
-                    , &[&city.name, &city.country,&city.state , &city.coord.lon, &city.coord.lat]).await?;
-            }
+            if let Err(err) = connection.execute("INSERT INTO cities (name , country , state , lon , lat ) VALUES ($1 , $2 , $3 , $4 , $5)"
+                    , &[&city.name, &city.country,&city.state , &city.coord.lon, &city.coord.lat]).await {
+                    log::error!("Duplicated entry, not unique: {:?}" , err);
+                }
         }
     }
     log::info!("Cities are in database");
