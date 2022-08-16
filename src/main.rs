7a2a4cfb3e34dfaa6159ffff5d75e2c263a47c6e
Ayrat Badykov <ayratin555@gmail.com>
2022-08-16 10:21:33 +0300

    add migrations with cities seeds

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-use weather_bot_rust::seeds;
 use weather_bot_rust::telegram::handler::Handler;
 use weather_bot_rust::workers;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-
-    seeds::insert_seeds().await.unwrap();
 
     workers::start_workers().await;
 
