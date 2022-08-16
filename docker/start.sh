b1969f365d59808d7e9f1391864349ade9c6fa03
pxp9 <48651252+pxp9@users.noreply.github.com>
2022-08-16 18:38:53 +0200

    start container script

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env sh
 
-    echo "Setting the database"
-    ./diesel database setup
 
-    ./weather_bot_rust
+if [ -z SET_DB ]; then
+  echo "Setting the database"
+  ./diesel database setup
+fi
+
+if [ -z REVERT_MIGRATION ]; then
+  echo "Revert to last migration"
+  ./diesel migration revert
+fi
+
+echo "Running Bot"
+./weather_bot_rust
