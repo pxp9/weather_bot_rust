cbcd0b609ebeb2816c46bce5361a432303248a3f
pxp9 <48651252+pxp9@users.noreply.github.com>
2022-08-18 20:10:31 +0200

    Finally docker compose is set, it was hard xd

--- docker/start.sh
+++ docker/start.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
+echo "set db value :$SET_DB"
 
-if [ -z SET_DB ]; then
+if [ ! -z $SET_DB ]; then
   echo "Setting the database"
   ./diesel database setup
 fi
 
-if [ -z REVERT_MIGRATION ]; then
+if [ ! -z $REVERT_DB ]; then
   echo "Revert to last migration"
   ./diesel migration revert
 fi
